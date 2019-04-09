837839b2f91682896a284874ccb119fc433a23a8
Arthur <arthur.souza.93@gmail.com>
2019-04-08 23:22:33 -0300

    Remove log on StockHelper

--- app/components/Stock/helper.js
+++ app/components/Stock/helper.js
@@ -74,7 +74,7 @@ const StockHelper = {
           }
 
           foundedStock.totalOrders = totalOrders;
-          console.log(foundedStock);
+
           callback(err, foundedStock);
         }
       );
